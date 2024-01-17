04b543b2c5efa15aa53e3701f85f95b8a9710c05
AleksandraMN <al_mn@mail.ru>
2024-01-17 22:03:16 +1000

    Lesson10.1

--- cart.js
+++ cart.js
@@ -1,55 +1,56 @@
 'use strict';
 
 const cart = {
-   items: [],
-   totalPrice: 0,
-   count: 0,
-   
-   get priceT() {
-      this.calculateItemPrice();
-      return this.totalPrice;
-   },
+  items: [],
+  totalPrice: 0,
+  count: 0,
 
-   set priceT(sum) {
-      return this.totalPrice = sum;
-   },
+  get priceT() {
+    this.calculateItemPrice();
+    return this.totalPrice;
+  },
 
-   add(x, y, z){
-      const obj = {'nameProduct': x, 'price': y, 'count': z};
-      const array = this.items.push(obj);
-      this.calculateItemPrice();
-      this.increaseCount(z);
-      return array;
-   },
+  set priceT(sum) {
+    this.totalPrice = sum;
+  },
 
-   increaseCount(z){
-      return this.count = this.count + z;
-   }, 
+  add(x, y, z) {
+    const obj = {'nameProduct': x, 'price': y, 'count': z};
+    const array = this.items.push(obj);
+    this.calculateItemPrice();
+    this.increaseCount(z);
+    return array;
+  },
 
-   calculateItemPrice(){
-      const array = [];
-      const prices = this.items.map((item) => {
-         const {
-            price,
-         } = item;
-         return array.push(price);
-      });
-      const sum = array.reduce((acc, item) => {
-         return acc + item;
-      }, 0);
-      return this.priceT = sum;
-   },
+  increaseCount(z) {
+    return this.count += z;
+  },
 
-   clear(){
-      return this.items.splice(0, this.items.length), this.totalPrice = 0, this.count = 0;
-   },
+  calculateItemPrice() {
+    const array = [];
+    let sum = 0;
+    this.items.map((item) => {
+      const {price} = item;
+      return array.push(price);
+    });
+    array.reduce((acc, item, array) => {
+      sum = acc + item;
+      return sum;
+    }, 0);
+    return this.priceT = sum;
+  },
 
-   print(){
-      const str = JSON.stringify(this.items);
-      console.log('Корзина ', str);
-      console.log('Общая стоимость товаров в корзине = ', this.priceT);
-      console.log('Общее количество товаров в корзине = ', this.count);
-   },
+  clear() {
+    return this.items.splice(0, this.items.length),
+    this.totalPrice = 0, this.count = 0;
+  },
+
+  print() {
+    const str = JSON.stringify(this.items);
+    console.log('Корзина ', str);
+    console.log('Общая стоимость товаров в корзине = ', this.priceT);
+    console.log('Общее количество товаров в корзине = ', this.count);
+  },
 };
 
 
@@ -62,6 +63,6 @@ cart.add('масло', 420, 1);
 cart.print();
 
 cart.totalPrice = 10;
-//cart.clear();
+// cart.clear();
 cart.print();
 
--- task02.js
+++ task02.js
@@ -1,19 +1,19 @@
 'use strict';
 
 const arr = [...listIPv4];
-//const arr = [].concat(listIPv4);
-//const arr = listIPv4.slice();
+// const arr = [].concat(listIPv4);
+// const arr = listIPv4.slice();
 
 
-const getNumber = function (arr) {
-   const newArr = arr.flat(Infinity);
-   const newArray = [...new Set(newArr)] ;
+const getNumber = function(arr) {
+  const newArr = arr.flat(Infinity);
+  const newArray = [...new Set(newArr)];
 
-   let number = 0;
-   for (let i = 0; i < newArray.length; i++) {
-   number += i;
-   }
-   return number;
+  let number = 0;
+  for (let i = 0; i < newArray.length; i++) {
+    number += i;
+  }
+  return number;
 };
 
 console.log(getNumber(arr));
